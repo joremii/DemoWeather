a63a94c03c7c2b9df88ce06cd3e49e537139fbfe
joremii <22022505@rp.edu.sg>
2023-08-18 12:16:32 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22022505/demoweather/Weather.java
@@ -0,0 +1,31 @@
+package sg.edu.rp.c346.id22022505.demoweather;
+
+public class Weather {
+    private String area;
+    private String forecast;
+
+    public Weather(String area, String forecast) {
+        this.area = area;
+        this.forecast = forecast;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public String getForecast() {
+        return forecast;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public void setForecast(String forecast) {
+        this.forecast = forecast;
+    }
+
+    public String toString() {
+        return "Weather{" + "area=" + area + '\'' + ", forecast='" + forecast + '\'' + '}';
+    }
+}
